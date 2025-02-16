557000be01f9f94ffac0d9be2fbcc7efe061f615
Tharunkumar2024 <tharunkumar.developers@gmail.com>
2025-02-16 15:45:54 +0530

    "Implemented the setup templates using Jinja and added the advanced configurations."

--- setup.py
+++ setup.py
@@ -6,15 +6,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastapi_boilerplate_kit',
-    version='0.0.1',
+    version='1.x.x',
     packages=find_packages(),
+    python_requires='>=3.10',
+    package_data={
+        'fastapi_boilerplate_kit': ['templates/*.jinja'],
+    },
+    include_package_data=True,
     py_modules=['cli'],
     install_requires=[
         'Click',
+        'jinja2',
     ],
     entry_points='''
         [console_scripts]
-        fastapi-boilerplate-kit=cli:cli
+        dnd=fastapi_boilerplate_kit.cli:cli
     ''',
     author='Tharunkumar Saravanan',
     author_email='tharunkumar.developers@gmail.com',
